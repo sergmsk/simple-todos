4b10e3e35783fe291f0d998b624172d705317ad0
KS <me@gmail.com>
2017-04-07 21:52:10 +0300

    task.js - добавление обработчика собития для кнопки

---
+++ imports/ui/task.js
@@ -0,0 +1,17 @@
+﻿import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.events({
+    'click .toggle-checked'() {
+        // Установить свойство в противоположность от его текущего значения
+        Tasks.update(this._id, {
+            $set: { checked: !this.checked },
+        });
+    },
+    'click .delete'() {
+        Tasks.remove(this._id);
+    },
+});
